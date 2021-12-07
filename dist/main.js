ecc94411c7c0464fc25320cd58c1edabcfb7c7b2
faroky4 <faroky4@gmail.com>
2021-12-07 12:05:39 +0200

    update the style

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,8 @@ const city = new CityModel()
 const rend = new Renderer()
 
 const loadPage = async function(){
+    $('.data-div').empty()
+
     await city.getDataFromDB()
     rend.render(city.getCitiesArr());
 }
@@ -29,7 +31,7 @@ $(document).on('click','#del-btn',function() {
     let relevantCityName= $(this).closest(".city-div").find(".nameOf-city")[0].innerHTML
     console.log(relevantCityName);
     city.removeCity(relevantCityName)
-
+    loadPage()
 })
 
 loadPage()
